568c8f1615233f127af4aa48d2619311e829a518
Qi <mail@qichen.me>
2019-01-14 23:30:33 -0500

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do |i|
+  Product.create(title: "product#{i}", price: i*10.1, inventory_count: 100)
+end
+
+Cart.create(total_value: 0, is_completed: false)
+
+
